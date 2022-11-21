7710dc7091e9c14774cedb06cdc786b487ae5497
Jo <dec@GentleStorms-Air.local>
2022-11-21 15:57:54 +0100

    renamed all folder names to lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,18 @@ import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Transporter from "./pages/Transporter/TransporterDashboard";
 import Client from "./pages/Client/ClientDashboard";
+import PreRegister from "./pages/Auth/PreRegister/Index";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route index path="/" element={<h1>Home</h1>} />
+        <Route path="/auth/">
+          <Route path="login" element={<Login />} />
+          <Route path="pre-register" element={<PreRegister />} />
+          <Route path="register" element={<Register />} />
+        </Route>
         <Route path="/admin" element={<Admin />} />
         <Route path="/client" element={<Client />} />
         <Route path="/transporter" element={<Transporter />} />
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,9 +5,11 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     colors: {
-      "neutral-90": "#211814",
-      "neutral-60": "#666666",
-      "neutral-25": "#BFBFBF",
+      neutral: {
+        90: "#211814",
+        60: "#666666",
+        25: "#BFBFBF",
+      },
       warning: "#FFD970",
       success: "#51D899",
       error: "#DE330D",
