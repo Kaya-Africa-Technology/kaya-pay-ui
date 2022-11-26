05baeaa1b774e071030a102892ce44d9e44f4e65
Jo <dec@GentleStorms-Air.local>
2022-11-26 21:33:04 +0100

    Feature(frontend): sign up page
    Worked on the sign up screen and I made some changes to the login and reset password pages.
    [starts #183838935]

--- src/pages/auth/registeruser.jsx
+++ src/pages/auth/registeruser.jsx
@@ -1,5 +1,73 @@
+import { Link } from "react-router-dom";
+import AuthSideImage from "../../Components/Shared/AuthSideImage";
+import TextInputField from "../../components/Shared/Textinput/TextInputField";
+
 const RegisterUser = () => {
-  return <h1>Registration Page</h1>;
+  return (
+    <div className="flex h-screen bg-neutral-25">
+      <div className="relative flex-1">
+        <div className="absolute top-5 left-5">
+          <img src="/kaya_logo.svg" alt="Kaya logo" className="w-18" />
+        </div>
+        <div className="flex h-screen items-center justify-center align-middle">
+          <div className="flex w-11/12 flex-col space-y-6 md:w-3/5">
+            <h2 className=" text-center text-3xl font-bold">Sign Up</h2>
+            <p className="text-center ">Start your journey with us.</p>
+            <div className="login-form">
+              <form className="flex flex-col gap-y-4">
+                <div className="flex flex-col justify-center space-y-0 lg:flex-row lg:items-center lg:space-x-5 lg:space-y-0">
+                  <TextInputField
+                    name="FirstName"
+                    id="firstName"
+                    placeholder="Enter your first name"
+                    type="text"
+                    label="First Name"
+                    cut
+                  />
+                  <TextInputField
+                    name="lastName"
+                    id="lastName"
+                    placeholder="Enter your last name"
+                    type="text"
+                    label="Last Name"
+                  />
+                </div>
+                <TextInputField
+                  name="email"
+                  id="email"
+                  placeholder="Enter your email Address"
+                  type="email"
+                  label="Email Address"
+                />
+                <TextInputField
+                  name="password"
+                  id="password"
+                  type="password"
+                  label="Password"
+                />
+                <p className="text-left text-xs">
+                  by clicking sign up you agree to the terms and conditions
+                </p>
+                <button className="btn-primary w-full" type="submit">
+                  Sign up
+                </button>
+              </form>
+              <p className="text-center">
+                Already have an account?{" "}
+                <Link className="link" to="/auth/login">
+                  Sign in
+                </Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <AuthSideImage
+        image="/images/truck2.jpg"
+        text="We provide exceptional customer experience with hassle-free booking, real-tracking and automated dispatch."
+      />
+    </div>
+  );
 };
 
 export default RegisterUser;
--- src/pages/auth/resetpassword.jsx
+++ src/pages/auth/resetpassword.jsx
@@ -3,31 +3,35 @@ import TextInputField from "../../components/Shared/Textinput/TextInputField";
 
 const ResetPassword = () => {
   return (
-    <section className="flex h-screen flex-col ">
-      <img className="ml-10 mt-5 w-24" src="/kaya_logo.svg" alt="kaya logo" />
-      <div className="flex h-screen items-center justify-center">
-        <div className="w-1/3 rounded-md  bg-neutral-25 px-20 py-20">
-          <h2 className="mb-10 text-center font-lato text-2xl font-extrabold ">
-            Reset your Password
-          </h2>
-          <form className="flex flex-col content-between justify-between">
-            <TextInputField
-              label="Email Address"
-              placeholder="Enter your Email Address to get a link to reset your Password"
-            />
-            <button className="btn-primary" type="submit">
-              Send reset password link
-            </button>
-          </form>
-          <p className="text-center">
-            Remember your password?{" "}
-            <Link className="link" to="/auth/login">
-              Login
-            </Link>
-          </p>
+    <div className="flex h-screen">
+      <div className="relative flex-1">
+        <div className="absolute top-5 left-5">
+          <img src="/kaya_logo.svg" alt="Kaya logo" className="w-18" />
+        </div>
+        <div className="flex h-screen items-center justify-center align-middle">
+          <div className="w-full rounded-md  bg-neutral-25 px-10 py-10 sm:w-11/12 md:w-3/5 lg:w-1/2">
+            <h2 className="mb-6 text-center text-3xl font-bold">
+              Reset your Password
+            </h2>
+            <form className="flex flex-col content-between justify-between">
+              <TextInputField
+                label="Email Address"
+                placeholder="Enter your Email Address to get a link to reset your Password"
+              />
+              <button className="btn-primary" type="submit">
+                Send reset password link
+              </button>
+            </form>
+            <p className="text-center">
+              Remember your password?{" "}
+              <Link className="link" to="/auth/login">
+                Login
+              </Link>
+            </p>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
